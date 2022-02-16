a37141bd2323dc71600566a8546a6a742f33652b
G3 <tuxgal@Baymax.area51>
2022-02-15 20:35:21 -0800

    Change Fatal color to bold red instead.

--- colors.go
+++ colors.go
@@ -2,7 +2,7 @@ package zzzlog
 
 var (
 	colorLevelStr = []string{
-		"\x1b[31mFATAL\x1b[0m",
+		"\x1b[1;31mFATAL\x1b[0m",
 		"\x1b[31mERROR\x1b[0m",
 		"\x1b[33mWARN \x1b[0m",
 		"\x1b[34mINFO \x1b[0m",
