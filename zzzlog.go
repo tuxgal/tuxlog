4707df818abc51462807a2f47881b02a1d386b32
G3 <tuxgal@Baymax.area51>
2022-02-15 19:16:10 -0800

    Move color related definitions into a separate file.

--- zzzlog.go
+++ zzzlog.go
@@ -17,14 +17,6 @@ const (
 )
 
 var (
-	colorLevelStr = []string{
-		"\x1b[31mFATAL\x1b[0m",
-		"\x1b[31mERROR\x1b[0m",
-		"\x1b[33mWARN \x1b[0m",
-		"\x1b[34mINFO \x1b[0m",
-		"\x1b[32mDEBUG\x1b[0m",
-		"\x1b[35mTRACE\x1b[0m",
-	}
 	defaultFormat = []string{
 		"",
 		"%v",
